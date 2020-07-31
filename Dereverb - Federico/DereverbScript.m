000eb82885fb4cbb497dbd7751d47c9263fdf59a
Federico Maver <maver.federico@gmail.com>
2020-07-31 15:05:50 +0200

    File iniziali
    2 cartelle una per federico una per filippo di partenza

---
+++ Dereverb - Federico/DereverbScript.m
@@ -0,0 +1,11 @@
+%% SASP PROJECT: Dereverb
+
+%% Part 1: Generation of reverberated sounds
+
+% Generation of RIR
+
+% Signals to be reverberated
+
+% Reverberation
+
+%% Part 2: Dereverberation algorithm
